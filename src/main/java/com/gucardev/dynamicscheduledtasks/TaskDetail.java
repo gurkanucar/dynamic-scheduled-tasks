b8efee2e42ce0bbca5b30d3f394715f8da3fe2bb
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-05-28 01:11:03 +0300

    feat: TaskDetail created

---
+++ src/main/java/com/gucardev/dynamicscheduledtasks/TaskDetail.java
@@ -0,0 +1,20 @@
+package com.gucardev.dynamicscheduledtasks;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TaskDetail {
+  private String content;
+  private long time;
+  private TimeType timeType;
+  private LocalDateTime exactTime;
+}
---
+++ src/main/java/com/gucardev/dynamicscheduledtasks/TimeType.java
@@ -0,0 +1,7 @@
+package com.gucardev.dynamicscheduledtasks;
+
+public enum TimeType {
+  SECONDS,
+  MINUTES,
+  EXACT_TIME
+}
