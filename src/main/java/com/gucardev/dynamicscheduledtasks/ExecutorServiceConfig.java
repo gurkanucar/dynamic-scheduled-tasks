3cc45ec22b28402b905eaf590e3409bc23b082a7
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-05-28 01:11:08 +0300

    feat: ExecutorServiceConfig created

---
+++ src/main/java/com/gucardev/dynamicscheduledtasks/ExecutorServiceConfig.java
@@ -0,0 +1,15 @@
+package com.gucardev.dynamicscheduledtasks;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ExecutorServiceConfig {
+
+  @Bean
+  ScheduledExecutorService scheduledExecutorService() {
+    return Executors.newScheduledThreadPool(10);
+  }
+}
