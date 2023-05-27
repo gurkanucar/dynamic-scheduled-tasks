f9ed46fda781408a2bf6b3359fc16609da2d76c1
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-05-28 01:09:16 +0300

    style: code formatted

--- src/main/java/com/gucardev/dynamicscheduledtasks/DynamicScheduledTasksApplication.java
+++ src/main/java/com/gucardev/dynamicscheduledtasks/DynamicScheduledTasksApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DynamicScheduledTasksApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DynamicScheduledTasksApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DynamicScheduledTasksApplication.class, args);
+  }
 
 }
