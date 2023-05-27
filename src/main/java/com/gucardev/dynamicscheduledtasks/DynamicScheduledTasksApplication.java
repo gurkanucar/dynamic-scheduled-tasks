4d65b7f2ac60a4f6fe4ff82a35020dda607b4b7c
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-05-28 00:58:21 +0300

    Initial commit

---
+++ src/main/java/com/gucardev/dynamicscheduledtasks/DynamicScheduledTasksApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.dynamicscheduledtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicScheduledTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicScheduledTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gucardev/dynamicscheduledtasks/DynamicScheduledTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.dynamicscheduledtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicScheduledTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
